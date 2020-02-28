03a4f5a302230a8b8ad001f15a9cca3725f1448f
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-02-28 16:55:00 +0100

    Correction of variable name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var PokeRepository = [
+var pokeRepository = [
   {name:'Rapidash', height:1.7, types:['fire']},
   {name:'Pikachu', height:0.4, types:['electric']},
   {name:'Jigglypuff', height:0.5, types:['fairy','normal']}
