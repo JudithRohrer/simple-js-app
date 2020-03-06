6bf149e2df0706fb85dda9efcfa342542b9ae451
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-03-06 10:28:17 +0100

    improvement of code readability

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var pokeRepository = [
 ];
 
 for(var i = 0; i < pokeRepository.length; i++) {
-  
+
     if (i == 0) {
     document.write ('<p>' + pokeRepository[i].name + ' (height: ' + pokeRepository[i].height +') - Wow, that\'s big!</p>')
   } else {
