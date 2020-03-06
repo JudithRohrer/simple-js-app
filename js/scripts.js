bc1c64bcc9fa8b5d58e83f6e60fce3cdde5419d8
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-03-06 10:02:38 +0100

    Set up for new the simple-app-projekt
    - folder structure
    - document completion
    - several img files import

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ var pokeRepository = [
   {name:'Pikachu', height:0.4, types:['electric']},
   {name:'Jigglypuff', height:0.5, types:['fairy','normal']}
 ];
+
+/* for(var i = 0; i < pokeRepository.length; i++) {
+    document.write ('<p>' + pokeRepository.name + ' (height: ' + pokeRepository.height + ' )</p>')
+    if (i == 0) {
+    document.write ('<p>' + pokeRepository.name + ' (height: ' + pokeRepository.height +' ) - Wow, that\'s big!</p>')
+  }
+}
+ */
