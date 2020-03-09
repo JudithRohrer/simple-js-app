4d98f4e871cbe1e9c2013bdf0055f2475b322e25
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-03-09 16:14:06 +0100

    wrapping repository into an IIFE
    - additionaly commented out old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-var pokeRepository = [
+var pokeRepository = (function (){
+  var repository = [
   {name:'Rapidash', height:1.7, types:['fire']},
   {name:'Pikachu', height:0.4, types:['electric']},
   {name:'Jigglypuff', height:0.5, types:['fairy','normal']}
 ];
+})();
 
-pokeRepository.forEach(function(arrayItem) {
+/* pokeRepository.forEach(function(arrayItem) {
   document.write ('<p>' + arrayItem.name + ' (height: ' + arrayItem.height + ')');
   if (arrayItem.height > 1.0) document.write (' - Wow, that\'s big!');
   document.write ('</p>');
-});
+});*/
