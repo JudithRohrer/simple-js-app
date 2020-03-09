726b64b5de114b119f3ce8adb9df312b939b8cba
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-03-09 17:05:13 +0100

    IIFE defined & adaption of loop syntax

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,24 @@ var pokeRepository = (function (){
   {name:'Rapidash', height:1.7, types:['fire']},
   {name:'Pikachu', height:0.4, types:['electric']},
   {name:'Jigglypuff', height:0.5, types:['fairy','normal']}
-];
+  ];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
 })();
 
-/* pokeRepository.forEach(function(arrayItem) {
+pokeRepository.getAll().forEach(function(arrayItem) {
   document.write ('<p>' + arrayItem.name + ' (height: ' + arrayItem.height + ')');
   if (arrayItem.height > 1.0) document.write (' - Wow, that\'s big!');
   document.write ('</p>');
-});*/
+});
